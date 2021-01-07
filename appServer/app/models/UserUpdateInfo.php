140a896b039c9f09ee3bee6996a19d2d89dcae81
Eduardo <eduardo.gheleno@gmail.com>
2021-01-07 19:16:47 -0300

    Filtering changes on UserUpdate info

--- appServer/app/models/UserUpdateInfo.php
+++ appServer/app/models/UserUpdateInfo.php
@@ -8,6 +8,8 @@ class UserUpdateInfo {
     private $email;
     private $phoneNumber;
 
+    static $errors = array();
+
     public function setId($id) {
         $this->id = $id;
     }
@@ -26,25 +28,31 @@ class UserUpdateInfo {
 
     public function update() {
         if (isset($this->userName)):
-            try {
-                $sql = "UPDATE users SET displayName = '$this->userName' WHERE id = '$this->id'";
-                $stmt = Connection::getConn()->prepare($sql);
-                $stmt->execute();
-            } catch(PDOException $e) {
-                echo 'Error! '.$e->getMessage();
-            }
+            if (mb_strlen($this->userName) >= 3 && mb_strlen($this->userName) <=15):
+                try {
+                    $sql = "UPDATE users SET displayName = '$this->userName' WHERE id = '$this->id'";
+                    $stmt = Connection::getConn()->prepare($sql);
+                    $stmt->execute();
+                } catch(PDOException $e) {
+                    echo 'Error! '.$e->getMessage();
+                }
+            else:
+                array_push(self::$errors, 'invalid-username');
+            endif;
         endif;
 
         if (isset($this->email)):
-            try {
-                $sql = "UPDATE users SET email = '$this->email' WHERE id = '$this->id'";
-                $stmt = Connection::getConn()->prepare($sql);
-                $stmt->execute();
-
-                echo 'Passed!';
-            } catch(PDOException $e) {
-                echo 'Error! '.$e->getMessage();
-            }
+            if (mb_strlen($this->email) > 3):
+                try {
+                    $sql = "UPDATE users SET email = '$this->email' WHERE id = '$this->id'";
+                    $stmt = Connection::getConn()->prepare($sql);
+                    $stmt->execute();
+                } catch(PDOException $e) {
+                    echo 'Error! '.$e->getMessage();
+                }
+            else:
+                array_push(self::$errors, 'invalid-email');
+            endif;
         endif;
 
         if (isset($this->phoneNumber)):
@@ -52,11 +60,16 @@ class UserUpdateInfo {
                 $sql = "UPDATE users SET phoneNumber = '$this->phoneNumber' WHERE id = '$this->id'";
                 $stmt = Connection::getConn()->prepare($sql);
                 $stmt->execute();
-
-                echo 'Passed!';
             } catch(PDOException $e) {
                 echo 'Error! '.$e->getMessage();
             }
+        endif;
+
+        if (empty(self::$errors)):
+            echo 'changes-saved';
+        else:
+            http_response_code(400);
+            echo json_encode(self::$errors);
         endif;
     }
 }
